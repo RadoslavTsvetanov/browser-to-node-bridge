0b78c852dbb34303c0016d11e836abba75055dd4
RadoslavTsvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2025-07-06 15:34:35 +0300

    fixing git shit

--- packages/non-domain-specific/http/Blaze/blaze-minimal-lib/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export  { Extended as app } from "./src/core/websocket/server/app/extended";
-
-export { HookBuilder } from "./src/core/websocket/server/utilites/builders/HookBuilder";
-
-export { MessageThatCanBeReceivedBuilder } from "./src/core/websocket/server/utilites/builders/MessageBuilder";
-
--- packages/non-domain-specific/http/Blaze/blaze-minimal-lib/lib/tests/integration/testing_if_websocket_works_as_expected/weboscket/main.ts
+++
@@ -1,5 +0,0 @@
-import { Port } from "@blazyts/better-standard-library";
-
-import { router } from "../../../dummmies/app";
-
-router.start(new Port(4000));
