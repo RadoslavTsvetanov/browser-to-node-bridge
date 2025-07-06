5a083b869d656165f60f218a807cd675e3137cb8
RadoslavTsvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2025-07-06 15:04:53 +0300

    fixed git bs

--- packages/http-bridge/src/client.ts
+++ packages/http-bridge/src/client.ts
@@ -1,4 +1,4 @@
 import type { Bridge } from "@blazyts/browser-to-node-bridge";
 import { RemoteServerBridge } from "@blazyts/server-proxy-implementation";
 
-export const bridge: Bridge = (new RemoteServerBridge())
+export const bridge: Bridge = (new RemoteServerBridge() )
